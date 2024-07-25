b4034a27b9f25b84ac2fb45b1a016556c4a0e828
Khairul Hidayat <khairul.hidayat@jkt1.ebdesk.com>
2024-07-25 20:34:10 +0700

    [BUG] config webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -52,6 +52,10 @@ const config: Configuration = {
           },
         },
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
     ],
   },
   resolve: {
